aa68a3ddaa8872a2c7059a0d9097742f63fec399
bsharma07 <sharmab07@gmail.com>
2024-02-14 14:09:53 -0500

    Update AddToCard.tsx

--- components/AddToCard.tsx
+++ components/AddToCard.tsx
@@ -2,46 +2,47 @@
 
 
 import { Product } from "../typings/productTyping"
-//import { useCartStore } from "../store"
+import { useCartStore } from "../store"
 import { Button } from "@/components/ui/button";
 import RemoveFromCart from "./RemoveFromCart";
 
 const AddToCard = ({ product }: { product: Product }) => {
-   // const [cart, AddToCard] = useCartStore((state) => [
-     //   state.cart,
-      //  state.addToCart,
-    //]);
+   const [cart, AddToCard] = useCartStore((state) => [
+       state.cart,
+       state.addToCart,
+    ]);
 
 
     // const howManyInCart = cart.filter(
     //     (item) => item.meta.sku === product.meta.sku
     // ).length;
 
-    // console.log("how many in cart", howManyInCart);
+    const howManyInCart = 4;
+    console.log("how many in cart", howManyInCart);
 
-    // const handleAdd = () => {
-    //     console.log("Adding to cart", product);
-    //    // AddToCard(product);
-    // };
-
-
-
-//     if (howManyInCart > 0) {
-//         return (
-//             <div className="flex space-x-5 items-center ">
-//                 <RemoveFromCart product={product}/>
-// {/* <p>-</p> */}
-//                 <span>{howManyInCart}</span>
-
-//                 <Button className="bg-blue-500 hover:bg-blue-700" onClick={handleAdd}> + </Button>
-//             </div>
-//         );  }
+    const handleAdd = () => {
+        console.log("Adding to cart", product);
+       // AddToCard(product);
+    };
 
 
 
-   // return (
-        // <Button  onClick={handleAdd}>AddToCard</Button>
-    //)
+    if (howManyInCart > 0) {
+        return (
+            <div className="flex space-x-5 items-center ">
+                {/* <RemoveFromCart product={product}/> */}
+{/* <p>-</p> */}
+                <span>{howManyInCart}</span>
+
+                <Button className="bg-blue-500 hover:bg-blue-700" onClick={handleAdd}> + </Button>
+            </div>
+        );  }
+
+
+
+   return (
+        <Button  onClick={handleAdd}>AddToCard</Button>
+    )
 }
 
 export default AddToCard
